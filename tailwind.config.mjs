ffb571120c7b8959d71a8c28b5f7b9033e8f7e85
Christian Diaz <christian@cdiaz.cloud>
2024-09-27 10:28:23 -0700

    Init NextUI/Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+const { nextui } = require( '@nextui-org/react' );
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
+}
